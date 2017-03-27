1d16d0f3e6d352cc4e01325cddddee6633732fa8
pszaro <github@szaroconsulting.com>
2017-03-27 07:27:18 -0400

    Removed package installs, will utilize Ansible to install across PI's

--- pi_image.sh
+++ pi_image.sh
@@ -12,40 +12,6 @@ sudo aptitude -y dist-upgrade
 sudo aptitude -y autoclean
 sudo aptitude -y full-upgrade
 
-echo "Installing Raspberry Pi Packages"
-sudo aptitude -y libraspberrypi-bin libraspberrypi0 raspberrypi-bootloader libraspberrypi-doc libraspberrypi-dev
-
-echo "Installing Base Packages"
-sudo aptitude -y install libgl1-mesa-dri libparse-debianchangelog-perl uuid-runtime xfonts-base gnupg-curl oss-compat upower libreadline5 sysv-rc-conf sysstat
-
-echo "Installing Multimedia Packages"
-#sudo aptitude -y install xrdp ffmpeg pulseaudio-esound-compat moc
-
-echo "Installing Productivity Packages"
-sudo aptitude -y install chromium-browser 
-
-echo "Installing USB WLAN Firmware"
-sudo aptitude -y install zd1211-firmware firmware-ralink firmware-realtek
-
-echo "Installing Web Servers"
-sudo aptitude -y install apache2 lighttpd 
-
-echo "Installing Standard Tools"
-sudo aptitude -y install dnsutils whois bluetooth
-
-echo "Installing Development Environment"
-sudo aptitude -y install vim git git-all 
-
-echo "Installing System/Security"
-sudo aptitude -y install tcpdump nmap python-nmap
-
-echo "Installing Ansible"
-#Add following to /etc/apt/sources.list
-# "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-#sudo apt-get update
-#sudo apt-get install ansible
-
 # Increase history
 echo "HISTSIZE=5000" >> /root/.bashrc
 
